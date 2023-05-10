dcdbd5805c1f394984a1801f52589d9cb0b7b754
brozzelerro <121559398+brozzelerro@users.noreply.github.com>
2023-05-10 19:32:24 +0300

    Update models.py
    1

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,1 +1,80 @@
+from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название',
+        help_text='Выберите категорию'
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name='Слаг',
+    )
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Название',
+        help_text='Выберите жанр'
+    )
+    slug = models.SlugField(max_length=20,
+                            unique=True, 
+                            verbose_name='Слаг')
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Название',
+        help_text='Выберите название произведения'
+    )
+    year = models.IntegerField(
+        verbose_name='Год',
+        null=True,
+        blank=True
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name='Категория',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='titles'
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        blank=True,
+        verbose_name='Жанр',
+        related_name='titles'
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='Описание')
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
