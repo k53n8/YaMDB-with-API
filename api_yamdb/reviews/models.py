4661769c6989d69725cb364374b70627b4a6f1db
Олег Иванов <ksengod@yandex.ru>
2023-05-10 21:57:58 +0500

    Review model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+from users.models import User
 
 
 class Category(models.Model):
@@ -16,3 +19,44 @@ class Category(models.Model):
 
     def __str__(self):
         return self.slug
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Произведение'
+    )
+    text = models.TextField(
+        verbose_name='Текст',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Автор отзыва'
+    )
+    score = models.PositiveIntegerField(
+        validators=[
+            MinValueValidator(1, 'Выберите значение от 1 до 10'),
+            MaxValueValidator(10, 'Выберите значение от 1 до 10')
+        ],
+        verbose_name='Рейтинг'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации отзыва'
+    )
+
+    class Meta:
+        ordering = ['pub_date']
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title', 'author'],
+                name='unique_review'
+            ),
+        ]
