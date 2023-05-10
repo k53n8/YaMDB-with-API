51981581f903398eb932cbb7dd6406657f794097
Алексей Мартынов <serov.smoke@yandex.ru>
2023-05-10 20:37:52 +0300

    1

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 from django.conf import settings
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название',
+        help_text='Выберите категорию'
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name='Слаг'
+    )
+
+    def __str__(self):
+        return self.slug
